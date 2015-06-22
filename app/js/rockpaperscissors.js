132b3f2f4491b1d408170eb59bab4580de286bd0
Michelle Maltbia <maltbia.michelle@gmail.com>
2015-06-21 23:34:41 -0700

    Updated homework assignment.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -37,7 +37,6 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
@@ -52,4 +51,3 @@ function playToFive() {
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
 }
-
